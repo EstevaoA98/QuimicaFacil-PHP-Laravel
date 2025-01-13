424dff10baf0f6f77a1c8204a1a0af14544b8b4f
Estevao Miranda <estevao.a98@gmail.com>
2025-01-12 21:57:06 -0300

    resolvendo erro na hora de enviar o formulario

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ContactMessage;
+use App\Models\collaborate;
 use App\Models\Home;
-use Illuminate\Http\Client\Request;
+use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
@@ -16,26 +18,37 @@ class SiteController extends Controller
 
     public function collaborate()
     {
+        $collaborate =collaborate::where('id', 1)->first();
         return view('site.collaborate');
     }
 
     public function send(Request $resquest)
     {
         $resquest->validate([
-            'name' => 'reuired|max:255',
-            'email' => 'required|max:255',
-            'subject'=> 'required|max:255',
-            'message' => 'required|max:255',
+            'name' => 'required|max:50',
+            'email' => 'required|max:50',
+            'subject'=> 'required|max:20',
+            'message' => 'required|max:300',
             'phone' => 'required|max:13',
         ],
         [
             'name.required' => 'O campo nome é obrigatório.',
+            'name.max'=>'O campo nome deve ter no máximo 50 caracteres.',
+
             'email.required' => 'O campo e-mail é obrigatório.',
+            'email.max'=>'O campo email deve ter no máximo 50 caracteres',
+
             'subject.required' => 'O campo assunto é obrigatório.',
+            'subject.max'=>'O campo assunto deve ter no máximo 200 caracteres',
+
             'message.required' => 'O campo mensagem é obrigatório.',
+            'message.max'=>'O campo mensagem deve ter no máximo 300 caracteres',
+
             'phone.required' => 'O campo telefone é obrigatório.',
             
+            
         ]);
-        return redirect()->back()->with('success', 'Formulário enviado com sucesso!');
+       
+        return back()->with('success', 'Formulário enviado com sucesso!');
     }
 }
--- app/Models/MessagesContact.php
+++ app/Models/MessagesContact.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class MessagesContact extends Model
 {
-    protected $table = "homes";
+    protected $table = "messages_contact";
 
     protected $fillable = ['name', 'email', 'subject', 'message', 'phone'] ;
 }
