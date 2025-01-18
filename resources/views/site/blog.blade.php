600a2853452cd7ca4c67fad6e5f066d204767c13
Estevao Miranda <estevao.a98@gmail.com>
2025-01-18 14:00:48 -0300

    pag post/conteudo

--- resources/views/site/blog.blade.php
+++ resources/views/site/blog.blade.php
@@ -1,7 +1,213 @@
 @extends('layouts.site')
 @section('content')
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="blog.css" rel="stylesheet">
+    </head>
 
+    <body>
+        </header>
 
+        <main class="container">
+            <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+                <div class="col-lg-6 px-0">
+                  <img src="img/imgPost/qui-sustent-post.jpg" alt="quimica meio ambiente" class="img-fluid">
+                    <h1 class="display-4 fst-italic">Química e Sustentabilidade</h1>
+                    <h2>A Química que Salva o Planeta</h2>
+                    <p class="lead my-3">A química é uma grande aliada na busca por um futuro sustentável. Um exemplo é a criação de plásticos biodegradáveis, que se decompõem rapidamente e ajudam a reduzir o impacto ambiental. Outro avanço importante é o desenvolvimento de métodos químicos para capturar e armazenar dióxido de carbono da atmosfera, ajudando a combater as mudanças climáticas. No campo da energia, a química está por trás das células solares mais eficientes, que convertem a luz do sol em eletricidade limpa. Além disso, novas tecnologias de purificação de água estão permitindo o acesso a água potável em regiões carentes. Este post aborda como a química está na linha de frente das soluções ambientais, mostrando que ciência e sustentabilidade andam de mãos dadas.</p>
+                    <p class="lead mb-0"><a href="#" class="text-body-emphasis fw-bold">Continue reading...</a></p>
+                </div>
+            </div>
 
+            <div class="row mb-2">
+                <div class="col-md-6">
+                    <div
+                        class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <strong class="d-inline-block mb-2 text-primary-emphasis">World</strong>
+                            <h3 class="mb-0">Featured post</h3>
+                            <div class="mb-1 text-body-secondary">Nov 12</div>
+                            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural
+                                lead-in to additional content.</p>
+                            <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                                Continue reading
+                                <svg class="bi">
+                                    <use xlink:href="#chevron-right" />
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div
+                        class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div class="col p-4 d-flex flex-column position-static">
+                            <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
+                            <h3 class="mb-0">Post title</h3>
+                            <div class="mb-1 text-body-secondary">Nov 11</div>
+                            <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to
+                                additional content.</p>
+                            <a href="#" class="icon-link gap-1 icon-link-hover stretched-link">
+                                Continue reading
+                                <svg class="bi">
+                                    <use xlink:href="#chevron-right" />
+                                </svg>
+                            </a>
+                        </div>
 
+                    </div>
+                </div>
+            </div>
+
+            <div class="row g-5">
+                <div class="col-md-8">
+                    <h3 class="pb-4 mb-4 fst-italic border-bottom">
+                        From the Firehose
+                    </h3>
+
+                    <article class="blog-post">
+                        <h2 class="display-5 link-body-emphasis mb-1">Experimento Químico: <br> Crescendo Cristais de
+                            Sulfato de Cobre</h2>
+                        <p class="blog-post-meta">2021 by <a href="https://edu.rsc.org" target="_blank" rel="external">Royal
+                                Society of Chemistry </a></p>
+
+                        <strong>
+                            <p>Objetivo:
+                                Demonstrar o processo de cristalização por evaporação e obter cristais visíveis de sulfato
+                                de cobre.</p>
+                        </strong>
+                        <h1>Materiais Necessários</h1>
+                        <p>
+                        <h3>1. Reagentes:</h3>
+                        <ul>
+                            <li>Sulfato de cobre pentahidratado () - Pode ser encontrado em lojas de produtos químicos ou de
+                                jardinagem (como fungicida).</li>
+                            <li>CuSO4⋅5H2OCuSO_4 \cdot 5H_2O</li>
+                            <li>Água destilada (para evitar impurezas que interfiram na cristalização). </li>
+                        </ul> <br>
+                        <h3>2. Equipamentos:</h3>
+                        <ul>
+                            <li>Béquer ou copo de vidro resistente ao calor.</li>
+                            <li>Bastão de vidro ou colher de madeira para mexer.</li>
+                            <li>Fogareiro ou aquecedor elétrico (nunca direto no fogo).</li>
+                            <li>Placa de Petri ou pires (para permitir a evaporação).</li>
+                            <li>Fio de náilon ou barbante (para servir como base para os cristais).</li>
+                            <li>Suporte para pendurar o fio (palito ou lápis que fique na borda do copo).</li>
+                        </ul>
+                        <br>
+                        <hr>
+
+                        <h1>Preparação e Instruções</h1>
+                        <h3>1. Dissolução:</h3>
+                        <ul>
+                            <li>Aqueça cerca de 200 ml de água destilada até que esteja quente, mas não fervendo.</li>
+                            <li>Adicione lentamente sulfato de cobre à água quente, mexendo até que não se dissolva mais
+                                (solução saturada). O líquido deve ficar azul intenso.</li>
+                        </ul>
+                        <br>
+                        <h3>2. Filtragem (opcional):</h3>
+                        <ul>
+                            <li>Se observar partículas não dissolvidas, coe a solução em um filtro de papel (por exemplo,
+                                filtro de café) para remover impurezas.</li>
+                        </ul>
+                        <br>
+                        <h3>3. Montagem do sistema de cristalização:</h3>
+                        <ul>
+                            <li>Transfira a solução para outro recipiente limpo, como um copo de vidro.</li>
+                            <li>Pendure um fio de náilon ou barbante preso a um palito ou lápis na boca do recipiente, de
+                                forma que fique suspenso dentro da solução sem tocar nas laterais.</li>
+                        </ul>
+                        <br>
+                        <h3>4. Evaporação controlada:</h3>
+                        <ul>
+                            <li>Deixe o recipiente em local seguro e sem vibrações, preferencialmente longe de luz solar
+                                direta.</li>
+                            <li>Após 24-48 horas, a evaporação da água levará à formação de cristais no fio e nas laterais
+                                do recipiente.</li>
+                        </ul>
+                        <br>
+                        <hr>
+                        <img src="img/imgPost/experiment-qui.webp" alt="">
+                        <hr>
+                        <br>
+                        <h3><strong>Dicas para Sucesso</strong></h3>
+                        <li><strong>Evaporação lenta</strong>: Coloque um pano poroso sobre o recipiente para evitar
+                            evaporação muito rápida e proteger contra poeira.</li>
+                        <li><strong>Controle de temperatura:</strong> Faça isso em um ambiente com temperatura estável para
+                            obter cristais maiores.</li>
+                        <li><strong>Repetição:</strong> Se desejar cristais maiores, remova os primeiros cristais formados e
+                            reintroduza-os na solução saturada como "sementes".
+                        </li>
+                        <hr>
+                        <h3><strong>Cuidados e Segurança</strong></h3>
+                        <li>Não ingira o sulfato de cobre. Ele é tóxico em contato com a pele ou se ingerido.</li>
+                        <li>Lave bem as mãos após manipular os reagentes.</li>
+                        <li>Use óculos de proteção ao aquecer a solução.</li>
+                        <hr>
+                        <h3><strong>Resultados Esperados</strong></h3>
+                        <li>Após alguns dias, você verá cristais azuis brilhantes crescendo no fio e nas laterais do
+                            recipiente.</li>
+                        <li>Você pode observar as faces geométricas dos cristais e entender o processo de organização das
+                            moléculas durante a cristalização.</li>
+                            <br>
+                </div>
+                <div class="col-md-4">
+                    <div class="position-sticky" style="top: 2rem;">
+                        <div class="p-4 mb-3 bg-body-tertiary rounded">
+                            <h4 class="fst-italic">About</h4>
+                            <p class="mb-0">Customize this section to tell your visitors a little bit about your
+                                publication, writers, content, or something else entirely. Totally up to you.</p>
+                        </div>
+                        <div>
+                            <h4 class="fst-italic">Recent posts</h4>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
+                                        href="#">
+                                        <svg class="bd-placeholder-img" width="100%" height="96"
+                                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+                                            preserveAspectRatio="xMidYMid slice" focusable="false">
+                                            <rect width="100%" height="100%" fill="#777" />
+                                        </svg>
+                                        <div class="col-lg-8">
+                                            <h6 class="mb-0">Example blog post title</h6>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
+                                        href="#">
+                                        <svg class="bd-placeholder-img" width="100%" height="96"
+                                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+                                            preserveAspectRatio="xMidYMid slice" focusable="false">
+                                            <rect width="100%" height="100%" fill="#777" />
+                                        </svg>
+                                        <div class="col-lg-8">
+                                            <h6 class="mb-0">This is another blog post title</h6>
+                                        </div>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top"
+                                        href="#">
+                                        <svg class="bd-placeholder-img" width="100%" height="96"
+                                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true"
+                                            preserveAspectRatio="xMidYMid slice" focusable="false">
+                                            <rect width="100%" height="100%" fill="#777" />
+                                        </svg>
+                                        <div class="col-lg-8">
+                                            <h6 class="mb-0">Longer blog post title: This one has multiple lines!</h6>
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </body>
+
+    </html>
 @endsection
